cbc056d6c56c43904dcae4b86abec00016943fb1
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-05-30 20:06:55 +0900

    stateの型

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState<any>('vvvvvv')
+
+  const onClick = () => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => setTodos('aaaaa'))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={onClick}>{todos}</button>
     </div>
   );
 }
